f600cf0986d18eef73c8ace9b0d2c1303bc60357
Jin <khjkhj1212@hanmail.net>
2021-06-16 00:12:31 +0900

    Create BO

---
+++ JPA_VUE_01/BO/jpabook/src/main/java/jpashops/jpabook/Member.java
@@ -0,0 +1,20 @@
+package jpashops.jpabook;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+}
---
+++ JPA_VUE_01/BO/jpashop/src/main/java/jbabook/jpashop/HelloController.java
@@ -0,0 +1,5 @@
+package jbabook.jpashop;
+
+public class HelloController {
+
+}
