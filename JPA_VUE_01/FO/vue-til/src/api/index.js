2cebc14ed83f448745c3731c50c9ba118e089865
Jin <khjkhj1212@hanmail.net>
2021-07-24 17:00:04 +0900

    gi

--- JPA_VUE_01/FO/vue-til/src/api/index.js
+++ JPA_VUE_01/FO/vue-til/src/api/index.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
-function createInstance() {}
+import { setInterceptors } from './common/interceptors';
+
+function createInstance() {
+  return axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+}
 
 // Axios 초기화
-function createInstanceWithAuth() {
-  const instance = axios.create({});
+function createInstanceWithAuth(url) {
+  const instance = axios.create({
+    baseURL: `${process.env.VUE_APP_API_URL}${url}`,
+  });
+
+  return setInterceptors(instance);
 }
 
 export const instance = createInstance();
-export const posts = createInstanceWithAuth();
+export const posts = createInstanceWithAuth('posts');
