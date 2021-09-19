360345863fac6768d5bc8bbc6d04b025a9a32dc2
Jin <khjkhj1212@hanmail.net>
2021-09-19 19:03:15 +0900

    [추가] BO 프로젝트 생성
    생성

---
+++ Vue/VUE-ENDGAME/vue-til-server/src/main/java/com/example/vuetilserver/VueTilServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuetilserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueTilServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueTilServerApplication.class, args);
+    }
+
+}
---
+++ Vue/VUE-ENDGAME/vue-til-server/src/test/java/com/example/vuetilserver/VueTilServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuetilserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueTilServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
